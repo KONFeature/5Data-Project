921fdfa5f0b525a373445a5e51ba1a4f42006655
romain <rvogelsperger@gmail.com>
2020-01-24 07:46:38 -0800

    ajout des filters pour r

--- shiny/mountpoints/apps/example-app/server.R
+++ shiny/mountpoints/apps/example-app/server.R
@@ -1,26 +1,75 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+library(ggvis)
+library(dplyr)
 
-library(shiny)
+# Set up handles to database tables on app start
+data <- iris
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
+axis_vars <- c(
+  "Tomato Meter" = "Meter",
+  "Numeric Rating" = "Rating",
+  "Number of reviews" = "Reviews",
+  "Dollars at box office" = "BoxOffice",
+  "Year" = "Year",
+  "Length (minutes)" = "Runtime"
+)
+
+
+function(input, output, session) {
+  
+  # Filter the movies, returning a data frame
+  movies <- reactive({
+    # Due to dplyr issue #318, we need temp variables for input values
+    slider1 <- input$slider1Title
+    minslider2 <- input$slider2Title[1]
+    maxslider2 <- input$slider2Title[2]
+
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # Apply filters
+    m <- data %>%
+      filter(
+        Param1 >= slider1,
+        Param2 >= minslider2,
+        Param2 <= maxslider2,
+      ) %>%
+      arrange(filter_data)
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # Optional: filter by select
+    if (input$selectTitle != "All") {
+      select <- paste0("%", input$selectTitle, "%")
+      m <- m %>% filter(Select %like% select)
+    }
+    # Optional: filter by director
+    if (!is.null(input$inputTitle) && input$inputTitle != "") {
+      InputTitle <- paste0("%", input$inputTitle, "%")
+      m <- m %>% filter(InputTitle %like% inputTitle)
+    }
+
     
+    m <- as.data.frame(m)
   })
   
-})
+  # A reactive expression with the ggvis plot
+  vis <- reactive({
+    # Lables for axes
+    xvar_name <- names(axis_vars)[axis_vars == input$xvar]
+    yvar_name <- names(axis_vars)[axis_vars == input$yvar]
+    
+    # Normally we could do something like props(x = ~BoxOffice, y = ~Reviews),
+    # but since the inputs are strings, we need to do a little more work.
+    xvar <- prop("x", as.symbol(input$xvar))
+    yvar <- prop("y", as.symbol(input$yvar))
+    
+    movies %>%
+      ggvis(x = xvar, y = yvar) %>%
+      layer_points(size := 50, size.hover := 200,
+                   fillOpacity := 0.2, fillOpacity.hover := 0.5,
+                   stroke = ~has_oscar, key := ~ID) %>%
+      add_axis("x", title = xvar_name) %>%
+      add_axis("y", title = yvar_name) %>%
+      set_options(width = 500, height = 500)
+  })
+  
+  vis %>% bind_shiny("plot1")
+  
+  output$n_movies <- renderText({ nrow(movies()) })
+}
--- shiny/mountpoints/apps/example-app/ui.R
+++ shiny/mountpoints/apps/example-app/ui.R
@@ -1,33 +1,45 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+library(ggvis)
 
-library(shiny)
+axis_vars <- c(
+  "Tomato Meter" = "Meter",
+  "Numeric Rating" = "Rating",
+  "Number of reviews" = "Reviews",
+  "Dollars at box office" = "BoxOffice",
+  "Year" = "Year",
+  "Length (minutes)" = "Runtime"
+)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+# For dropdown menu
+actionLink <- function(inputId, ...) {
+  tags$a(href='javascript:void',
+         id=inputId,
+         class='action-button',
+         ...)
+}
+
+fluidPage(
+  titlePanel("Title"),
+  fluidRow(
+    column(3,
+           wellPanel(
+             h4("Filter"),
+             sliderInput("slider1Title", "de minumum à ?",
+                         10, 300, 80, step = 10),
+             sliderInput("slider2Title", "de ? à ?",
+                         0, 800, c(0, 800), step = 1),
+             selectInput("selectTitle", "Selection avec plusieurs choix",
+                         c("All", "choix1", "choix2", "choix3")
+             ),
+             textInput("inputTitle", "inputTexte"),
+           ),
+           wellPanel(
+             selectInput("xvar", "X-axis variable", axis_vars, selected = "Meter"),
+             selectInput("yvar", "Y-axis variable", axis_vars, selected = "Reviews"),
+           )
     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+    # Nos graph et table
+    column(9,
+           ggvisOutput("plot1"),
     )
   )
-))
+)
