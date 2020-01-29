cfd924e61217eeb35aaab9f75c53b6a4aabf62e7
Quentin <quentin@nivelais.com>
2020-01-29 15:34:51 +0100

    Ajout R a spark & debut lien mongo

--- shiny/mountpoints/apps/project/server.R
+++ shiny/mountpoints/apps/project/server.R
@@ -1,40 +1,34 @@
 # Import them
 library(shiny)
 
-# set system variables ----
-# - location of Spark on master node;
-# - add sparkR package directory to the list of path to look for R packages
+# Prepare SparkR (define the bin location on the current system)
 Sys.setenv(SPARK_HOME="/opt/spark-2.4.4-bin-hadoop2.7")
 .libPaths(c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib"), .libPaths()))
 
-# Test a sparkR server
+# Import sparkR lib
 library(SparkR)
+
+# Prepare list of config and package for the SparkR Session
+sparkConfigList = list(
+  spark.executor.memory="1g",
+  spark.mongodb.input.uri="mongodb://127.0.0.1/data-projects.students?readPreference=primaryPreferred",
+  spark.mongodb.output.uri="mongodb://127.0.0.1/data-projects.students")
+
+sparkPackageList = c("org.mongodb.spark:mongo-spark-connector_2.11:2.4.1")
+
+# Init the sparkR session
 my_spark <- sparkR.session(
  master="spark://master:7077",
- sparkConfig=list(),
- appName="data-projects"
-)
+ sparkConfig=sparkConfigList,
+ appName="r-project",
+ sparkPackages = sparkPackageList)
 
+ # Copy iris database to mongo via spark (in theory)
+iris_tbl <- createDataFrame(iris)
+head(iris_tbl)
 
-# test <- read.text("s3a://adhoc.analytics.data/README.md")
-# head(collect(test))
-
-
+# Close the spark connection
 sparkR.session.stop()
-
-# Open spark connection
-# library(sparklyr)
-# sc <- spark_connect(master = "spark://localhost:7077")
-
-# # check the spark connection
-# connection_is_open(sc)
-
-# # Test random file load from spark
-# test <- spark_read_csv(sc, "test", "s3a://adhoc.analytics.data/README.md")
-# test
-
-# # Close spark connection
-# spark_disconnect_all()
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
