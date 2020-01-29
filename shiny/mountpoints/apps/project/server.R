dda3153d0db48e2562dd7538662ce15eefe3ae0e
Quentin <quentin@nivelais.com>
2020-01-29 14:35:08 +0100

    Liaison spark & r

--- shiny/mountpoints/apps/project/server.R
+++ shiny/mountpoints/apps/project/server.R
@@ -1,12 +1,40 @@
-
-# Install the required package
-install.packages(c("sparklyr", "rmarkdown", "ggplot2"))
-
 # Import them
 library(shiny)
 
-# Load SparkR library into your R session
+# set system variables ----
+# - location of Spark on master node;
+# - add sparkR package directory to the list of path to look for R packages
+Sys.setenv(SPARK_HOME="/opt/spark-2.4.4-bin-hadoop2.7")
+.libPaths(c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib"), .libPaths()))
+
+# Test a sparkR server
 library(SparkR)
+my_spark <- sparkR.session(
+ master="spark://master:7077",
+ sparkConfig=list(),
+ appName="data-projects"
+)
+
+
+# test <- read.text("s3a://adhoc.analytics.data/README.md")
+# head(collect(test))
+
+
+sparkR.session.stop()
+
+# Open spark connection
+# library(sparklyr)
+# sc <- spark_connect(master = "spark://localhost:7077")
+
+# # check the spark connection
+# connection_is_open(sc)
+
+# # Test random file load from spark
+# test <- spark_read_csv(sc, "test", "s3a://adhoc.analytics.data/README.md")
+# test
+
+# # Close spark connection
+# spark_disconnect_all()
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -18,15 +46,7 @@ shinyServer(function(input, output) {
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    # Load spark session
-    my_spark <- sparkR.session(
-      master="local[*]",
-      sparkConfig=list(),
-      appName="my_app"
-      )
-    
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')    
   })
   
 })
