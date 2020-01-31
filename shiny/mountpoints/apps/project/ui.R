38ca79a416280ba4a9bfde3ba13170370e0261ff
Quentin <quentin@nivelais.com>
2020-01-31 13:59:08 +0100

    Debut implementation shinydashboard

--- shiny/mountpoints/apps/project/ui.R
+++ shiny/mountpoints/apps/project/ui.R
@@ -1,29 +1,68 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
+# Libs to imports
 library(listviewer)
 library(shiny)
 library(httr)
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  # Sidebar with a slider input for number of bins 
+library(shinydashboard)
+
+# UI part of the application
+ui <- dashboardPage(
+    # Apply green skin
+  skin = "green",
+
+    # Header of the page
+  dashboardHeader(title = "Supinfo stats"),
+
+    # Left menu
+  dashboardSidebar(
+    # Search form
+    sidebarSearchForm(textId = "searchText", buttonId = "searchButton", label = "Search..."),
+    # Tabs
+    sidebarMenu(
+      menuItem("Table", tabName = "table", icon = icon("table")),
+      menuItem("Birth Date", tabName = "birth_date", icon = icon("birthday-cake"))
+    )
+    # Selectors
+
+  ),
+
+  # Content of the page
+  dashboardBody(
     
-    # Show a plot of the generated distribution
-    mainPanel(
-      # textOutput("schema")
-      # jsoneditOutput( "jsed" )
+    # Top row
+    fluidRow(
+      # Number of students
+      valueBox(10 * 2, "Students", icon = icon("list-ol")),
+      # Number of graduated students
+      valueBox(10 * 2, "Graduated students", icon = icon("stamp"), color = "green"),
+      # Percent of graduated students
+      valueBox(10 * 2, "Ratio of gradueted students", icon = icon("percentage"), color = "yellow")
+    ),
 
-             dataTableOutput('table')
+    # Tab part
+    tabItems(
+      # Table with all the data
+      tabItem(
+        tabName="table",
 
+        h1("Table with all the data we got"),
+
+        fluidRow(
+          box(
+            title = "Table of the data",
+            width = 12,
+            status = "success",
+            solidHeader = TRUE,
+            "Just all the data we have on supinfo students",
+            dataTableOutput('table')
+          )
+        )
+      ),
+
+
+      tabItem(
+        tabName="birth_date",
+        h1("test birth_date")
+      )
     )
   )
 )
